1495e4749ab64ea69cf5356eca87e6862ef6681b
RyujiDC <ddcuellarc@udistrital.edu.co>
2024-10-04 13:46:01 -0500

    Add files via upload

---
+++ Project_V1/training.py
@@ -0,0 +1,58 @@
+import random
+import json
+import pickle
+import numpy as np
+import nltk
+
+from nltk.stem import WordNetLemmatizer
+from keras.src.models import Sequential
+from keras.src.layers import Dense, Dropout, Activation
+from keras.src.optimizers import sgd
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+nltk.download('punkt_tab')
+nltk.download('wordnet')
+
+words=[]
+classes=[]
+documents=[]
+ignore_letters=['!',',','.']
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list=nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list,intent["tag"]))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+words=[lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+words=sorted(set(words))
+pickle.dump(words,open('words.pkl','wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+training=[]
+output_empty=[0]*len(classes)
+for document in documents:
+    bag=[]
+    word_patterns=document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+    output_row=list(output_empty)
+    output_row[classes.index(document[1])]=1
+    training.append([bag,output_row])
+random.shuffle(training)
+training= np.array(training)
+print(training)
+train_x=list(training[:,0])
+train_y=list(training[:,1])
+
+model = Sequential()
+model.add(Dense(units=128, activation='relu', input_shape=(len(train_x[0]),)))
+model.add(Dropout(0.5))
+model.add(Dense(units=64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+Optimizador=sgd.SGD(learning_rate=0.001,decay=1e-6,momentum=0.9,nesterov=True)
+model.compile(loss='categorical_crossentropy',optimizer=Optimizador,metrics=['accuracy'])
+train_process=model.fit(np.array(train_x),np.array(train_y),batch_size=5,epochs=150)
+model.save('Jhonrecipes_model.h5', train_process)
